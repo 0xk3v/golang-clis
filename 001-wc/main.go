ecd32d3c63b66248dc282bdbf5bcb8e2d2784c9a
0xk3v <kevin_@tuta.io>
2025-08-07 23:22:06 +0200

    added count function to count words, lines and bytes from a buffer

--- 001-wc/main.go
+++ 001-wc/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	lines := flag.Bool("l", false, "Count lines")
+	countBytes := flag.Bool("b", false, "Count bytes")
+	flag.Parse()
+
+	fmt.Println(count(os.Stdin, *lines, *countBytes))
+}
+
+func count(r io.Reader, countLines, countBytes bool) int {
+	// Scanner
+	scanner := bufio.NewScanner(r)
+
+	if countLines {
+		scanner.Split(bufio.ScanLines)
+	} else if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	} else {
+		scanner.Split(bufio.ScanWords)
+	}
+
+	// Word count
+	wc := 0
+
+	for scanner.Scan() {
+		wc++
+	}
+
+	return wc
 }
