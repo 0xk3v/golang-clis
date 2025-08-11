86cf3a387955fbd1672be43c8fd85388959999c5
0xk3v <kevin_@tuta.io>
2025-08-11 22:55:27 +0200

    added tests for Complete and Delete methods

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -18,3 +18,55 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
 	}
 }
+
+// TestComplete tests the Complete method of List type
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "Drink tea!"
+
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+
+	if l[0].Done {
+		t.Error("New task should not be completed")
+	}
+
+	l.Complete(1)
+
+	if !l[0].Done {
+		t.Error("New task should be completed")
+	}
+}
+
+// TestDelete tests the Delete method of List type
+func TestDelete(t *testing.T) {
+	l := todo.List{}
+
+	tasks := []string{
+		"Task one",
+		"Task two",
+		"Task three",
+	}
+
+	for _, v := range tasks {
+		l.Add(v)
+	}
+
+	if l[0].Task != tasks[0] {
+		t.Errorf("Expected %d, got %d instead.", 2, len(l))
+	}
+
+	l.Delete(2)
+
+	if len(l) != 2 {
+		t.Errorf("Expected list length %q, got %q instead.", tasks[0], l[0].Task)
+	}
+
+	if l[1].Task != tasks[2] {
+		t.Errorf("Expected %q, got %q instead.", tasks[2], l[1].Task)
+	}
+}
