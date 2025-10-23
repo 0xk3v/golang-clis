72083fb842b6f30afe3d8f3237d456758caa3a88
0xk3v <kevin_@tuta.io>
2025-10-23 21:38:59 +0200

    added run, parseContent and saveHTML functions

--- 003-mdp/main.go
+++ 003-mdp/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
 )
 
 const (
@@ -14,7 +19,6 @@ const (
 					<meta charset="UTF-8">
 					<meta name="viewport" content="width=device-width, initial-scale=1">
 					<title>Markdown Preview tool</title>
-					<link href="css/style.css" rel="stylesheet">
 				</head>
 				<body>
 	`
@@ -36,11 +40,53 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename); err != nil {
+	if err := run(*filename, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
-func run(filename string) error {
-	return nil
+func run(filename string, out io.Writer) error {
+	// Read all the data from the input file and check for errors
+
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+
+	if err := temp.Close(); err != nil {
+		return err
+	}
+
+	outName := temp.Name()
+	fmt.Fprintln(out, outName)
+
+	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	// Parse the markdown file through blackfriday and bluemonday
+	// to generate a valid and safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	// Create a buffer of bytes to write to a file.
+	var buffer bytes.Buffer
+
+	// Write html to bytes buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outputFile string, htmlData []byte) error {
+	return os.WriteFile(outputFile, htmlData, 0o644)
 }
