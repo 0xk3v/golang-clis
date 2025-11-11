8849aedf6b334c4043448493774a9cd884cceed9
0xk3v <kevin_@tuta.io>
2025-11-11 10:17:42 +0100

    updated the program to allow auto preview (on/off)

--- 003-mdp/main.go
+++ 003-mdp/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -32,6 +34,8 @@ const (
 func main() {
 	// Parse Arguments
 	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
+
 	flag.Parse()
 
 	// If user did not provide input, show usage
@@ -40,14 +44,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename, os.Stdout); err != nil {
+	if err := run(*filename, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
-func run(filename string, out io.Writer) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	// Read all the data from the input file and check for errors
-
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -67,7 +70,17 @@ func run(filename string, out io.Writer) error {
 	outName := temp.Name()
 	fmt.Fprintln(out, outName)
 
-	return saveHTML(outName, htmlData)
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	defer os.Remove(outName)
+
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -89,4 +102,33 @@ func parseContent(input []byte) []byte {
 
 func saveHTML(outputFile string, htmlData []byte) error {
 	return os.WriteFile(outputFile, htmlData, 0o644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	// Define executable based on OS
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	// Append filename to parameters slice
+	cParams = append(cParams, fname)
+
+	// Locate executable in PATH
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
+
+	// Open the file using default programs
+	return exec.Command(cPath, cParams...).Run()
 }
